2a67aa7d74fc4c52d89a2e27435d63deafa1f7a3
duncanhunter <duncan@duncanhunter.com.au>
2018-06-11 07:04:12 +0200

    add router effect for product category

--- libs/products/src/lib/+state/products.effects.ts
+++ libs/products/src/lib/+state/products.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { ProductsService } from './../services/products/products.service';
 import { ProductsActionTypes } from './../+state/products.actions';
-import { mergeMap, map, tap, catchError } from 'rxjs/operators';
+import { mergeMap, map, tap, catchError, filter } from 'rxjs/operators';
 import * as productActions from './../+state/products.actions';
 import { of } from 'rxjs';
 import { Product } from '@demo-app/data-models';
@@ -10,15 +10,20 @@ import { ROUTER_NAVIGATION, RouterNavigationAction } from '@ngrx/router-store';
 
 @Injectable()
 export class ProductsEffects {
-  @Effect({dispatch: false})
+  @Effect()
   loadFilteredProducts$ = this.actions$.pipe(
     ofType(ROUTER_NAVIGATION),
-    tap((r: RouterNavigationAction) => {
-      console.log('route effect', r.payload.routerState.root.queryParams['category']);
-      // debugger;
-    })
-  )
-
+    filter((r: RouterNavigationAction) => r.payload.routerState.url.startsWith('/products')),
+    map((r: RouterNavigationAction) => r.payload.routerState.root.queryParams['category']),
+    mergeMap((category: string) =>
+      this.productService
+        .getProducts(category)
+        .pipe(
+          map((products: Product[]) => new productActions.LoadProductsSuccess(products)),
+          catchError(error => of(new productActions.LoadProductsFail(error)))
+        )
+    )
+  );
 
   @Effect()
   loadProducts$ = this.actions$.pipe(
@@ -35,7 +40,6 @@ export class ProductsEffects {
         )
     )
   );
-
 
   constructor(
     private actions$: Actions,
